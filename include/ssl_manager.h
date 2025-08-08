97c8de007382f46ea05be7d7630df6d52e7a893d
David Andrew St John <davestj@gmail.com>
2025-08-07 23:11:26 -0700

    refactor rsa key generation to evp api

--- include/ssl_manager.h
+++ include/ssl_manager.h
@@ -40,7 +40,6 @@
 #include <openssl/x509.h>
 #include <openssl/x509v3.h>
 #include <openssl/pem.h>
-#include <openssl/rsa.h>
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 #include <openssl/bio.h>
